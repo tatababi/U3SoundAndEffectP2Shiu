bf8dcdfb6130662c124ed621ae1b8be4998fdbaf
DxDTitan <728472@student.dusd.net>
2025-04-24 09:57:12 -0700

    fixed errors
    had errors with player gravity and fixed the jump but background and player still do not move

--- P2SoundAndEffectShiu/Assets/Scripts/PlayerController.cs
+++ P2SoundAndEffectShiu/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float jumpForce;
+    public float jumpForce = 10;
     public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver;
+
 
 
     // Start is called before the first frame update
@@ -18,17 +21,35 @@ public class PlayerController : MonoBehaviour
 
 
     }
-    public bool isOnGround = true;
+  
+
     // Update is called once per frame
     void Update() {
 
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isOnGround = false; } }
+            isOnGround = false; }
+    }
 
+   
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true; }
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+          
+            Debug.Log("Game Over!");
+            gameOver = true;
+        }
+    }
+
+
+        
+            
+                
 }
     
 
--- P2SoundAndEffectShiu/Assets/Scripts/SpawnManager.cs
+++ P2SoundAndEffectShiu/Assets/Scripts/SpawnManager.cs
@@ -6,16 +6,31 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
-       
+     InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript =
+            GameObject.Find("Player").GetComponent<PlayerController>();
     }
-    
+
+    void SpawnObstacle()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+ 
     }
+
+   
 }
