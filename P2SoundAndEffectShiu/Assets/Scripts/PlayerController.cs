7d2064e246f4f3cdcac81bcf184685ef5d396844
DxDTitan <728472@student.dusd.net>
2025-05-08 08:31:14 -0700

    tralalalaleo

--- P2SoundAndEffectShiu/Assets/Scripts/PlayerController.cs
+++ P2SoundAndEffectShiu/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -17,10 +19,20 @@ public class PlayerController : MonoBehaviour
     public AudioClip jumpSound;
     private AudioSource playerAudio;
     public bool canDoubleJump = false;
+    public bool jumpOnce = false;
+    public bool jumpTwice = false;
+    public float speed = 1.0f;
+    public int score = 0;
+    public bool dashModeActivated = false;
+    [SerializeField] TextMeshProUGUI m_Object;
+
 
     public bool gameOver;
-    
 
+
+
+
+   
 
 
     // Start is called before the first frame update
@@ -34,8 +46,8 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    
-    
+
+
     // Update is called once per frame
     void Update()
     {
@@ -43,9 +55,9 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            playerAnim.speed =  1f;
-           
-           
+            playerAnim.speed = 1f;
+
+
 
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
@@ -62,17 +74,15 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetTrigger("Jump_trig");
             playerAudio.PlayOneShot(jumpSound, 1);
 
-            //sprint if
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                Time.timeScale = 2f;
-            }
-            else Time.timeScale = 1f;
+
+
 
         }
+
     }
 
-  
+
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
@@ -90,14 +100,45 @@ public class PlayerController : MonoBehaviour
             explosionParticle.Play();
             playerAudio.PlayOneShot(crashSound, 1.0f);
         }
+
+        
+        else if (Input.GetKeyDown(KeyCode.LeftShift) && isOnGround && !gameOver)
+        {
+            speed = 2.0f;
+            playerAnim.SetFloat("Speed_f", speed);
+            dashModeActivated = true;
+        }
+
+        else if (Input.GetKeyUp(KeyCode.LeftShift) && isOnGround && !gameOver)
+        {
+            speed = 1.0f;
+            playerAnim.SetFloat("Speed_f", speed);
+            dashModeActivated = false;
+
+        }
+        if (!gameOver)
+        {
+            score += (int)speed;
+            if (dashModeActivated)
+            {
+                score += 100;
+            }
+            m_Object.text = "Score:" + score;
+
+        }
+
+        }
     }
 
 
-        
-            
-                
-}
-    
 
-    
+
+
+
+
+
+
+
+
+
 
